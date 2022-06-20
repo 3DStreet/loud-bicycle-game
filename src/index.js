824c8f508940eed4f6eb7f5c45445f0cba45019f
Sloth Today <sloth.today22@gmail.com>
2022-06-20 02:01:11 +0200

    level movement and horn/shout buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 require('aframe');
+
+window.onload = (function() {
+    var hornEl = document.getElementById('horn');
+    var shoutEl = document.getElementById('shout');
+
+    hornEl.addEventListener('click', function() {
+        console.log('HORN!');
+    });
+    shoutEl.addEventListener('click', function() {
+        console.log('SHOUT!');
+    });
+});
