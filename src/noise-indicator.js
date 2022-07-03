43f674fe484fd55683e3234a702ae3929c1d82a0
Sloth Today <sloth.today22@gmail.com>
2022-07-03 19:18:57 +0200

    noise meter for horn

--- src/noise-indicator.js
+++ src/noise-indicator.js
@@ -4,21 +4,58 @@ import { GAME_STATE, GAME_STATES } from "./game-manager";
 AFRAME.registerComponent('noise-indicator', {
     init: function() {
         this.collider = this.el.components['aabb-collider'];
+        this.hornEl = document.getElementById('horn');
+        this.hornMeterEl = document.getElementById('horn-meter');
+        this.hornMeter = 0;
+        this.isActive = false;
+    
         window.addEventListener("keypress", this.onKeyPressed.bind(this));
+
+        setInterval(() => {
+            if (this.isActive) {
+                if (this.hornMeter > 0) {
+                    this.hornMeter -= 5;
+                    this.hornMeter = Math.max(0, this.hornMeter);
+                    this.updateMeter();
+                }
+            }
+            else {
+                this.hornMeter += 10;
+                this.hornMeter = Math.min(100, this.hornMeter);
+                this.updateMeter();
+            }
+        }, 300);
+    },
+    updateMeter: function() {
+        this.hornMeterEl.value = this.hornMeter;
+        if (this.hasLowMeter()) {
+            this.hornMeterEl.className = 'low-meter';
+            this.hornEl.classList.add('disabled');
+        } else {
+            this.hornMeterEl.className = 'high-meter';
+            this.hornEl.classList.remove('disabled');
+        }
+    },
+    hasLowMeter: function() {
+        return this.hornMeter < 50;
     },
     display: function(isSmall) {
         if(GAME_STATE !== GAME_STATES.PLAYING) return;
+
+        if (this.hasLowMeter()) return;
 
         isSmall ? this.scaleSmall() : this.scaleLarge();
 
         this.el.object3D.position.x = playerController.el.object3D.position.x;
         this.isActive = true;
         this.el.object3D.visible = true;
+        this.hornMeter -= isSmall ? 30 : 50;
+        this.updateMeter();
+        
         setTimeout(() => {
             this.el.object3D.visible = false;
             this.isActive = false;
         }, 1000);
-
     },
     onKeyPressed: function(e) {
         switch(e.key) {
