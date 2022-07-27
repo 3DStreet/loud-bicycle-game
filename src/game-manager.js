c4b7c0bf66505f46842cbf183385a75903b67383
Srile <florianisk@yahoo.com>
2022-07-27 16:22:42 +0200

    Add dynamic level generation

--- src/game-manager.js
+++ src/game-manager.js
@@ -1,3 +1,5 @@
+import { gameData } from "./level-data";
+
 export const GAME_STATES = {
     PLAYING: 0,
     END: 1,
@@ -20,11 +22,15 @@ AFRAME.registerComponent('game-manager', {
         setTimeout(() => {
             this.interactablePool = document.querySelector('[interactable-pool]').components['interactable-pool'];
             this.coinPool = document.querySelector('[coin-pool]').components['coin-pool'];
-            this.levelAnimation = document.querySelector('#level').components.animation;
+            this.level = document.querySelector('#level');
+            this.levelAnimation = level.components.animation;
             this.headerLabel = document.querySelector('#game-state-header');
             this.gameScoreLabel = document.querySelector('#score');
             this.playLevel();
             this.coinAudio = this.el.components.sound;
+
+            // TODO: Chosen by menu
+            this.generateLevel(0);
         }, 100);
     },
     stopLevel: function() {
@@ -48,4 +54,33 @@ AFRAME.registerComponent('game-manager', {
         this.gameScoreLabel.innerText = gameScore;
         this.coinAudio.playSound();
     },
+    generateLevel: function(index) {
+        const levelData = gameData.levels[index];
+        console.log('ld', levelData.streetUrls);
+        this.currentLevelStreetEls = []
+        let isLastStreet = false;
+        let spawnDistance = levelData.streetLength / 2;
+        for (let i = 0; i < 10; i++) {
+            const el = document.createElement('a-entity');
+
+            if(isLastStreet) {
+                el.setAttribute('position', {x: 2, y: -0.1, z: -(spawnDistance - levelData.streetWidth / 2)})
+                el.setAttribute('rotation', {x: -90, y: 0, z: 0})
+                el.setAttribute('geometry', {width: levelData.streetWidth, height: levelData.streetWidth, primitive: 'plane'})
+                el.setAttribute('material', `src:url(${levelData.intersectionUrls[0]})`)
+                spawnDistance += levelData.streetLength;
+            } else {
+                el.setAttribute('position', {x: 1.5, y: 0, z: -(spawnDistance - levelData.streetLength / 2)})
+                el.setAttribute('street', {length: levelData.streetLength})
+                el.setAttribute('streetmix-loader', {streetmixStreetURL: levelData.streetUrls[0]})
+                spawnDistance += levelData.streetWidth;
+            }
+            isLastStreet = !isLastStreet;
+            this.level.append(el);
+            this.currentLevelStreetEls.push(el);
+        }
+    }, 
+    removeLevel: function() {
+
+    }
 });
---
+++ src/level-data.js
@@ -0,0 +1,30 @@
+export const gameData = {
+    levels: [
+        {
+            name: 'Level 1 - Drop off the kids',
+            duration: 180,
+            streetLength: 30,
+            streetWidth: 30,
+            streetLengthAdditionalRange: 10,
+            streetUrls: [
+                'https://streetmix.net/LoudBicycle/8/street_3'
+            ],
+            intersectionUrls: [
+                'https://cdn.glitch.global/4cbfc52c-9f40-485c-b7ff-e3ed0494f9a0/intersection.jpeg?v=1650059985944'
+            ]
+        },
+        {
+            name: 'Level 2 - Get to work',
+            duration: 180,
+            streetLength: 100,
+            streetWidth: 45,
+            streetLengthAdditionalRange: 10,
+            streetUrls: [
+                'https://streetmix.net/LoudBicycle/8/street_3'
+            ],
+            intersectionUrls: [
+                'https://cdn.glitch.global/4cbfc52c-9f40-485c-b7ff-e3ed0494f9a0/intersection.jpeg?v=1650059985944'
+            ]
+        }
+    ]
+} 
