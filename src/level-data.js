8354cf99a46e1181b3373b7dbc71f55ddce1f791
Srile <florianisk@yahoo.com>
2022-08-10 15:17:19 +0200

    Add 3d bell to bike-train-member

--- src/level-data.js
+++ src/level-data.js
@@ -7,7 +7,7 @@ export const gameData = {
             streetWidth: 30,
             streetLengthAdditionalRange: 10,
             streetUrls: [
-                'https://streetmix.net/LoudBicycle/8/street_3'
+                'https://streetmix.net/florianisikci/1/street_3'
             ],
             intersectionUrls: [
                 'https://cdn.glitch.global/4cbfc52c-9f40-485c-b7ff-e3ed0494f9a0/intersection.jpeg?v=1650059985944'
