c1f70fa0572be24cc04b2e31f0bcf87beab59a7e
Jonathan Lansey <lansey+github@gmail.com>
2023-08-11 00:14:41 -0400

    icons rotate when in use, and rotate back to where they started

--- src/noise-meter.js
+++ src/noise-meter.js
@@ -41,6 +41,7 @@ AFRAME.registerComponent('noise-meter', {
     tick: function(_t, dt) {
         // if (GAME_STATE === GAME_STATES.PLAYING && this.noiseIndicator) {
         if (this.noiseIndicator) {
+            this.clickerEl.style.transform = `rotate(${this.meter - 100}deg)`;
             if(!this.displaying || this.broken) {
                 let increase = (dt * METER_INTERVAL_INCREASE)/METER_INTERVAL_MS;
                 // make it recharge more slowly if the ray meter is on
